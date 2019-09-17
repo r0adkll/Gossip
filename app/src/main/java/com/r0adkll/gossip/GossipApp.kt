06e68fa311e0c4cd6ebf80e278a848e94e160057
Drew Heavner <veedubusc@gmail.com>
2019-09-17 16:47:34 -0400

    Added app icon Added Firebase integrations Setup Koin DI Setup Theme / Colors

--- app/src/main/java/com/r0adkll/gossip/GossipApp.kt
+++ app/src/main/java/com/r0adkll/gossip/GossipApp.kt
@@ -1,11 +1,20 @@
 package com.r0adkll.gossip
 
 import android.app.Application
+import com.r0adkll.gossip.internal.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class GossipApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@GossipApp)
+            androidLogger()
+            modules(appModule)
+        }
 
 
     }
---
+++ app/src/main/java/com/r0adkll/gossip/internal/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.r0adkll.gossip.internal.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
